fa7ea76068e8dabd9d162ee7f2c23cc4a755042c
tero-yrjola <yrjola.tero@gmail.com>
2020-01-19 17:00:49 +0200

    Read status-file and convert it to a suitable form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {readDefaultStatusFile} from './statusFileHandler'
 import './App.css';
 
 function App() {
+    const [packages, setPackages] = useState({});
+    useEffect(() => {
+            readDefaultStatusFile().then(res => setPackages(res))
+        }, []);
     return (
         <div className="App">
+
         </div>
     );
 }
---
+++ src/statusFileHandler.js
@@ -0,0 +1,45 @@
+import mockStatus from './mockStatus'
+
+export function readDefaultStatusFile() {
+    return fetch(mockStatus)
+        .then(response => response.text())
+        .then(text => convertStatusFileToArray(text))
+}
+
+function convertStatusFileToArray(statusContent) {
+    const linesToKeep = ['Package', 'Description', 'Depends'];
+    const keyValueSeparator = ':';
+
+    const packageStrings = statusContent.split('\n\n');
+    const packagesWithFilteredLines = packageStrings
+        .map(packageString => packageString.split('\n')
+            .filter(packageLineString =>
+                linesToKeep.some(lineToKeep => packageLineString.startsWith(lineToKeep))
+            )
+        );
+
+    const arrayOfPackageObjects = packagesWithFilteredLines.map(packageArray => {
+        const packageLineObject = {};
+        packageArray.forEach(packageLine => {
+            const separatorIndex = packageLine.indexOf(keyValueSeparator);
+            const key = packageLine.substring(0, separatorIndex);
+            const value = packageLine.substring(separatorIndex + 1 + keyValueSeparator.length);
+            if (key === 'Depends')
+                packageLineObject[key] = cleanDependsLine(value);
+            else
+                packageLineObject[key] = value
+        });
+        return packageLineObject
+    });
+
+    return arrayOfPackageObjects.sort(comparePackageName)
+}
+
+function cleanDependsLine(line) {
+    const lineWithoutParentheses = line.replace('|',',').replace(/ *\([^)]*\) */g, "");
+    return [...new Set(lineWithoutParentheses.split(',').map(packageName => packageName.trim()))];
+}
+
+function comparePackageName(a, b) {
+    return a.Package && a.Package.toLowerCase() < b.Package && b.Package.toLowerCase ? -1 : 0
+}
