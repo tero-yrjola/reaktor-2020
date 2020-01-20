a1db8bed7f3c4bd6accafd0a0291e080f69ed233
tero-yrjola <yrjola.tero@gmail.com>
2020-01-20 09:57:04 +0200

    Add search functionality

--- src/components/PackageList.js
+++ src/components/PackageList.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import './PackageList.css'
 
 function PackageList({packages, setSelectedPackageId}) {
+    const [searchValue, setSearchValue] = useState("");
     return (
         <div id="package-list-container">
-            {packages && packages.map(p =>
-                <div className="package-list-item" key={p.id} onClick={() => setSelectedPackageId(p.id)}>
-                    <p>
-                        {p.Package}
-                    </p>
-                </div>)}
+            <>
+                <input id="package-search" type="text" placeholder="Search" value={searchValue}
+                       onChange={e => setSearchValue(e.target.value)}
+                />
+                <span id="clear-button" onClick={() => setSearchValue("")}>X</span>
+            </>
+            <div id="package-list">
+                {packages.length > 0 && packages.filter(p => p.Package.includes(searchValue)).map(p =>
+                    <div className="package-list-item" key={p.id} onClick={() => setSelectedPackageId(p.id)}>
+                        <p>
+                            {p.Package}
+                        </p>
+                    </div>)}
+            </div>
         </div>
     );
 }
